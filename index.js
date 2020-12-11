7f04c43f28cf2d76463896770409ad9680b70b07
Nandakishorpalei <nandakishorpalei7676@gmail.com>
2020-12-11 09:56:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -28,20 +28,20 @@ document.addEventListener("keypress", function(event) {
 function makesound(key){
   switch (key){
     case "W":
-      var audio=new Audio("sounds/WAJAH_TUM_HO_.mp3");
+      var audio=new Audio("WAJAH_TUM_HO_.mp3");
         audio.play();
     break;
 
     case "K":
 
-      var audio1=new Audio("sounds/Kabhi Aayine Pe.mp3");
+      var audio1=new Audio("Kabhi Aayine Pe.mp3");
       audio1.play();
       break;
 
 
     case "C":
 
-      var audio2=new Audio("sounds/chahunmainyaana.mp3");
+      var audio2=new Audio("chahunmainyaana.mp3");
       audio2.play();
 
     break;
@@ -49,58 +49,58 @@ function makesound(key){
 
     case "D":
 
-      var audio3=new Audio("sounds/Downtown.m4a");
+      var audio3=new Audio("Downtown.m4a");
       audio3.play();
     break;
 
 
     case "H":
 
-      var audio4=new Audio("sounds/Hai_Dil_Ye_Mera.mp3")
+      var audio4=new Audio("Hai_Dil_Ye_Mera.mp3")
       audio4.play();
     break;
 
     case "J":
 
-      var audio5=new Audio("sounds/Jashn-E-Ishqa.mp3");
+      var audio5=new Audio("Jashn-E-Ishqa.mp3");
       audio5.play();
     break;
 
     case "T":
 
-      var audio6=new Audio("sounds/Tum Hi Ho_.mp3");
+      var audio6=new Audio("Tum Hi Ho_.mp3");
       audio6.play();
 
     break;
 
     case "L" :
-    var audio14=new Audio("sounds/Laapata.mp3");
+    var audio14=new Audio("Laapata.mp3");
     audio14.play();
     break;
 
     case "B":
-    var audio15=new Audio("sounds/BurjKhalifa.mp3");
+    var audio15=new Audio("BurjKhalifa.mp3");
     audio15.play();
     break;
 
 // key section
 
     case "w":
-     alert(events + " have got clicked ")
-      var audio7=new Audio("sounds/WAJAH_TUM_HO_.mp3");
+    
+      var audio7=new Audio("WAJAH_TUM_HO_.mp3");
         audio7.play();
     break;
 
     case "k":
 
-      var audio8=new Audio("sounds/Kabhi Aayine Pe.mp3");
+      var audio8=new Audio("Kabhi Aayine Pe.mp3");
       audio8.play();
       break;
 
 
     case "c":
 
-      var audio9=new Audio("sounds/chahunmainyaana.mp3");
+      var audio9=new Audio("chahunmainyaana.mp3");
       audio9.play();
 
     break;
@@ -108,20 +108,20 @@ function makesound(key){
 
     case "d":
 
-      var audio10=new Audio("sounds/Downtown.m4a");
+      var audio10=new Audio("Downtown.m4a");
       audio10.play();
     break;
 
 
     case "h":
 
-      var audio11=new Audio("sounds/Hai_Dil_Ye_Mera.mp3")
+      var audio11=new Audio("Hai_Dil_Ye_Mera.mp3")
       audio11.play();
     break;
 
     case "j":
 
-      var audio12=new Audio("sounds/Jashn-E-Ishqa.mp3");
+      var audio12=new Audio("Jashn-E-Ishqa.mp3");
       audio12.play();
     break;
 
@@ -131,25 +131,25 @@ function makesound(key){
 
     case "t":
 
-      var audio13=new Audio("sounds/Tum Hi Ho_.mp3");
+      var audio13=new Audio("Tum Hi Ho_.mp3");
       audio13.play();
 
     break;
 
     case "l":
-    var audio16=new Audio("sounds/Laapata.mp3");
+    var audio16=new Audio("Laapata.mp3");
     audio16.play();
     break;
 
     case "b":
-    var audio17=new Audio("sounds/BurjKhalifa.mp3");
+    var audio17=new Audio("BurjKhalifa.mp3");
     audio17.play();
     break;
 
     case "N":
 
 
-    var audio15=new Audio("sounds/BurjKhalifa.mp3");
+    var audio15=new Audio("BurjKhalifa.mp3");
 
 
     audio15.pause();
